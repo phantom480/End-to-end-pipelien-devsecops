3063bf81eb6d996acece7869c3691c6eac004ef3
phantom480 <aarifalimohd043@gmail.com>
2026-06-07 18:29:25 +0000

    first commit

---
+++ scripts/jenkins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#this Script belong to Cloudaseem Youtube channel #####
+# jenkins installation on ubuntu 
+sudo apt update -y
+sudo apt install fontconfig openjdk-21-jre -y
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins 
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
